bffeb6ea52bd04588e136b4690810550bcdb8f51
Keyo <keyo@aiii.ai>
2019-10-02 22:39:34 +0800

    feat: add copyStringToClipboard function

--- popup.js
+++ popup.js
@@ -25,29 +25,45 @@ document.querySelector('.gallery_box').addEventListener('click', (_this) => {
     let a_element = _this.toElement.closest('a');
     if (a_element) {
         let clipBoardContent = _this.toElement.closest('a').querySelector('img').src;
-        // TODO 複製到剪貼簿
+        copyStringToClipboard(clipBoardContent);
+        // TODO 秀複製成功訊息
     }
 });
 
 let inject = (parentId, result) => {
-    console.log(result);
     var arr = result.filter(r => r.parentId == parentId);
     if (arr.length > 0) { document.querySelector('.gallery_box').innerHTML = ''; }
     for (const [index, item] of arr.entries()) {
         //Todo 圖多要Random
         if (index > 6) { break; }
-        //Todo 監聽事件 點擊複製
         var content = `
-                            <li>
-                                <a href="#0" class="imgur" ><img src="${item.url}">
-                                    <div class="box_data">
-                                        <span>${item.title}</span>
-                                    </div>
-                                </a>
-                            </li>`;
+                        <li>
+                            <a href="#" class="imgur" ><img src="${item.url}">
+                                <div class="box_data">
+                                    <span>${item.title}</span>
+                                </div>
+                            </a>
+                        </li>`;
         var temp = document.createElement('div');
         temp.innerHTML = content.trim();
         document.querySelector('.gallery_box').append(temp.firstChild);
     };
+}
+
+let copyStringToClipboard = (str)=> {
+    // Create new element
+    var el = document.createElement('textarea');
+    // Set value (string to be copied)
+    el.value = str;
+    // Set non-editable to avoid focus and move outside of view
+    el.setAttribute('readonly', '');
+    el.style = { position: 'absolute', left: '-9999px' };
+    document.body.appendChild(el);
+    // Select text inside element
+    el.select();
+    // Copy text to clipboard
+    document.execCommand('copy');
+    // Remove temporary element
+    document.body.removeChild(el);
 }
 
